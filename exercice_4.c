fc146a3d47e5de12589ab096961cac26cfe0f5d8
kappablanka <theo.piacentini@gmail.com>
2022-11-22 09:53:17 +0100

    fin exo 4

---
+++ exercice_4.c
@@ -0,0 +1,12 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char const *argv[])
+{
+    char char1 = "a";
+    char char2 = "a";
+    char char3 = "a";
+    printf("Entrez 3 char :");
+    scanf("%c%c%c", &char1, &char2, &char3);
+    return 0;
+}
