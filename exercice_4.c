45323abed0507f1a21deffad9ef4588a07a5a478
kappablanka <theo.piacentini@gmail.com>
2022-11-22 09:53:23 +0100

    a

--- exercice_4.c
+++ exercice_4.c
@@ -3,10 +3,11 @@
 
 int main(int argc, char const *argv[])
 {
-    char char1 = "a";
-    char char2 = "a";
-    char char3 = "a";
-    printf("Entrez 3 char :");
-    scanf("%c%c%c", &char1, &char2, &char3);
+    char char1 = 'a';
+    char char2 = 'a';
+    char char3 = 'a';
+    printf("Entrez 3 chars : ");
+    scanf("%c %c %c", &char1, &char2, &char3);
+    printf("vos 3 chars sont : %c %c %c ", char1, char2, char3);
     return 0;
 }
