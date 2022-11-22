31457703ae7c0b054bd2954664d72427476601da
kappablanka <theo.piacentini@gmail.com>
2022-11-22 09:14:33 +0100

    fin exo 3

--- exercice_3.c
+++ exercice_3.c
@@ -1,2 +1,11 @@
 #include <stdlib.h>
 #include <stdio.h>
+
+int main(int argc, char const *argv[])
+{
+    int age = 0;
+    printf("quel est votre age ? : ");
+    scanf("%d", &age);
+    printf("votre age est : %d", age);
+    return 0;
+}
